f69a92624f7e1fe27167388e34311ec3fec70492
sanokoh <nishiki.drop@gmail.com>
2024-11-22 17:20:10 +0900

    役員プロフィール

--- page-message.php
+++ page-message.php
@@ -126,7 +126,7 @@ get_header();
 			</div>
 			<div class="message__contents sp-only">
 				<?php foreach ($executives as $executive): ?>
-					<div id="<?php echo $executive['id']-; ?>-sp" class="content">
+					<div id="<?php echo $executive['id']; ?>-sp" class="content">
 						<div class="executive-container">
 							<div class="executive container">
 								<h2 class="heading">
