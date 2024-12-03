e4aaa462758eb7f7ad655f4719182b95760bfd39
sanokoh <nishiki.drop@gmail.com>
2024-12-03 14:19:06 +0900

    heroのmax-height設定

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+<div class="notfound">
+	<h1>404 NOT FOUND</h1>
+	<p>ご指定のページが見つかりませんでした。<br>お手数おかけいたしますが、サイリーグページのトップ、<br class="pc">または上部のメニューよりお探しいただきますようお願いいたします。</p>
+
+	<p class="back -center">
+		<a href="/"><img src="/assets/images/common/arrow.svg" class="desvg" alt="">サイトトップへ戻る</a>
+	</p>
+
+
+</div>
+<?php get_footer(); ?>
+
